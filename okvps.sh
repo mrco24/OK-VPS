19143dc4c89233a3617a1d692f020a6cceab952b
mrco24 <95030367+mrco24@users.noreply.github.com>
2022-05-01 14:31:36 +0600

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -25,13 +25,13 @@ ENVIRONMENT () {
 	#Specific Kali Linux
 		#Specificity : no package name with "python"
         apt-get update -y > /dev/null 2>&1;
-        apt-get install chromium python3 python3-pip unzip make gcc libpcap-dev curl build-essential libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev ruby libgmp-dev zlib1g-dev -y > /dev/null 2>&1;
+        apt-get install python3 python3-pip unzip make gcc libpcap-dev curl build-essential libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev ruby libgmp-dev zlib1g-dev -y > /dev/null 2>&1;
         cd /tmp && curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py > /dev/null 2>&1 && python2 get-pip.py > /dev/null 2>&1;
         pip install -U setuptools > /dev/null 2>&1;
 }
 
 
-unset OS
+  Go(){
 	#Bash colors
 	sed -i '/^#.*force_color_prompt/s/^#//' ~/.bashrc && source ~/.bashrc
 	echo -e ${BLUE}"[ENVIRONMENT]" ${GREEN}"Packages required installation is done !"; echo "";
