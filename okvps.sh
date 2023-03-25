bb5f75cbe3cab60ab3574b535af07261ed1c001e
mrco24 <95030367+mrco24@users.noreply.github.com>
2023-03-25 19:18:00 +0600

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -26,15 +26,11 @@ ENVIRONMENT () {
 }
 
 
-  Go(){
-	#Bash colors
-	#Generic fot both OS - Golang environment
-	echo -e ${BLUE}"[ENVIRONMENT]" ${RED}"Golang environment installation in progress ...";
-	cd /root/OK-VPS/tools/file && wget https://go.dev/dl/go1.19.5.linux-amd64.tar.gz && tar -zxvf go1.19.5.linux-amd64.tar.gz -C /usr/local/ && mkdir ~/.go && GOROOT=/usr/local/go && GOPATH=~/.go && PATH=$PATH:$GOROOT/bin:$GOPATH/bin && update-alternatives --install "/usr/bin/go" "go" "/usr/local/go/bin/go" 0 && update-alternatives --set go /usr/local/go/bin/go;
-	echo -e ${BLUE}"[ENVIRONMENT]" ${GREEN}"Golang environment installation is done !"; echo "";
-}
-
 SUBDOMAINS_ENUMERATION () {
+       #Subfinder
+	echo -e ${BLUE}"[SUBDOMAINS ENUMERATION]" ${RED}"Golang installation in progress ...";
+	cd /root/OK-VPS/tools/file && wget https://go.dev/dl/go1.19.5.linux-amd64.tar.gz && tar -zxvf go1.19.5.linux-amd64.tar.gz -C /usr/local/ && mkdir ~/.go && GOROOT=/usr/local/go && GOPATH=~/.go && PATH=$PATH:$GOROOT/bin:$GOPATH/bin && update-alternatives --install "/usr/bin/go" "go" "/usr/local/go/bin/go" 0 && update-alternatives --set go /usr/local/go/bin/go;
+	echo -e ${BLUE}"[SUBDOMAINS ENUMERATION]" ${GREEN}"Golang installation is done !"; echo "";
 	#Subfinder
 	echo -e ${BLUE}"[SUBDOMAINS ENUMERATION]" ${RED}"Subfinder installation in progress ...";
 	GO111MODULE=on go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest > /dev/null 2>&1 && ln -s ~/go/bin/subfinder /usr/local/bin/;
